5c91ec7bb8771cbe6b3b072106148f1d70c5668f
fxhmyz <alifahmy99111@gmail.com>
2024-09-21 09:50:28 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,48 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    all_books = []
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        Book.all_books.append(self)
+
+    def __repr__(self):
+        return f"<Book: {self.title}, Author: {self.author}, Pages: {self.pages}>"
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and value:
+            self._title = value
+        else:
+            raise ValueError("Title must be a non-empty string.")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, str) and value:
+            self._author = value
+        else:
+            raise ValueError("Author must be a non-empty string.")
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        if isinstance(value, int) and value > 0:
+            self._pages = value
+        else:
+            raise ValueError("Pages must be a positive integer.")
+
+    @classmethod
+    def get_all_books(cls):
+        return cls.all_books
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,48 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    all_shoes = []
+
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        Shoe.all_shoes.append(self)
+
+    def __repr__(self):
+        return f"<Shoe: {self.brand}, Size: {self.size}, Color: {self.color}>"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and value:
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a non-empty string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._size = value
+        else:
+            raise ValueError("Size must be a positive number.")
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        if isinstance(value, str) and value:
+            self._color = value
+        else:
+            raise ValueError("Color must be a non-empty string.")
+
+    @classmethod
+    def get_all_shoes(cls):
+        return cls.all_shoes
